5d826f68e3a1bf036af764041fdf797be251dc2e
Tomáš Kout <kout.tk@gmail.com>
2017-05-08 09:52:22 +0200

    Get selector from options
    For compatibility with jQuery 3, from https://github.com/brutaldesign/swipebox/pull/298/commits/56840634b5887b88cb60f90949d34ec13b0936ab?diff=split

--- src/js/jquery.swipebox.js
+++ src/js/jquery.swipebox.js
@@ -31,13 +31,13 @@
 				loopAtEnd: false,
 				autoplayVideos: false,
 				queryStringData: {},
-				toggleClassOnLoad: ''
+				toggleClassOnLoad: '',
+				selector: null
 			},
 
 			plugin = this,
 			elements = [], // slides array [ { href:'...', title:'...' }, ...],
-			$elem,
-			selector = elem.selector,
+			$elem,			
 			isMobile = navigator.userAgent.match( /(iPad)|(iPhone)|(iPod)|(Android)|(PlayBook)|(BB10)|(BlackBerry)|(Opera Mini)|(IEMobile)|(webOS)|(MeeGo)/i ),
 			isTouch = isMobile !== null || document.createTouch !== undefined || ( 'ontouchstart' in window ) || ( 'onmsgesturechange' in window ) || navigator.msMaxTouchPoints,
 			supportSVG = !! document.createElementNS && !! document.createElementNS( 'http://www.w3.org/2000/svg', 'svg').createSVGRect,
@@ -83,7 +83,7 @@
 
 			} else {
 
-				$( document ).on( 'click', selector, function( event ) {
+				$( elem ).on( 'click', plugin.settings.selector, function( event ) {
 
 					// console.log( isTouch );
 
@@ -92,10 +92,12 @@
 						return false;
 					}
 
-					if ( ! $.isArray( elem ) ) {
-						ui.destroy();
-						$elem = $( selector );
-						ui.actions();
+					ui.destroy();
+ 
+ 					if ( plugin.settings.selector === null ) {
+ 						$elem = $( elem );
+ 					} else {
+ 						$elem = $( elem ).find( plugin.settings.selector );
 					}
 
 					elements = [];
@@ -113,9 +115,7 @@
 					}
 
 					if ( relVal && relVal !== '' && relVal !== 'nofollow' ) {
-						$elem = $( selector ).filter( '[' + relType + '="' + relVal + '"]' );
-					} else {
-						$elem = $( selector );
+						$elem = $elem.filter( '[' + relType + '="' + relVal + '"]' );
 					}
 
 					$elem.each( function() {
